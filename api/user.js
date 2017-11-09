95b4b3f01c63d62a49a45672e80f15146c1571d6
NickWu <wujunao19950715@gmail.com>
2017-11-09 14:30:11 +0000

    Add endpoint for user signup.

--- api/user.js
+++ api/user.js
@@ -1,12 +1,39 @@
 var user_db = require('../db/user-quiries');
 var bcrypt = require('bcryptjs');
 
-// function userSignUp(req, res, next) {
-//     const username = req.body.username;
-//     const email = req.body.email;
-//     const password = req.body.password;
+function userSignUp(req, res, next) {
+    const username = req.body.username;
+    const email = req.body.email;
+    const password = req.body.password;
 
-//     // Hashing
-//     const salt = bcrypt.genSaltSync();
-//     const hash = bcrypt.hashSync(password, salt);
-// }
+    // Check if username is used.
+    user_db.getUserByUsername(username, result => {
+        if (result.userId) {
+            res.status(409)
+                .json({
+                    status: 'error',
+                    message: 'Username used'
+                });
+            return;
+        }
+    });
+
+    // Hashing
+    const salt = bcrypt.genSaltSync();
+    const hash = bcrypt.hashSync(password, salt);
+    console.log(hash);
+
+    user_db.createUser(username, email, hash, result => {
+        if (result.status == 'success') {
+            res.status(200)
+                .json(result.message);
+        } else {
+            res.status(500)
+                .json(result);
+        }
+    })
+}
+
+module.exports = {
+    userSignUp: userSignUp
+}
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var user_api = require('../api/user');
+
+router.post('/api/user/signup', user_api.userSignUp);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
