fec66aa130be54ffe86395655dd363d71b0c068c
NickWu <wujunao19950715@gmail.com>
2017-12-31 10:12:09 -0500

    Using manuall print statement to make compile error works.

--- api/compile.js
+++ api/compile.js
@@ -1,7 +1,8 @@
 const fs = require('fs-extra')
 const pf = require('portfinder')
 const fileDB = require('../db/file-quiries')
-const { spawn } = require('child_process')
+const { execFile } = require('child_process')
+var spawn = require('child-process-promise').spawn
 
 module.exports.createConfigFile = username => {
   return pf.getPortPromise()
@@ -50,21 +51,77 @@ module.exports.compileLinksFile = function (req, res, next) {
         module.exports.linxProc.kill()
       }
 
-      module.exports.linxProc = spawn('linx', [`--config=tmp/${username}_config`, `tmp/${username}_source.links`])
+      module.exports.linxProc = spawn('linx',
+        [`--config=tmp/${username}_config`, `tmp/${username}_source.links`],
+        { capture: [ 'stdout', 'stderr' ] })
 
-      module.exports.linxProc.stderr.on('data', (data) => {
-        console.log(data.toString())
-        module.exports.linxProc.kill()
-        res.status(500).json({
-          status: 'compile error',
-          message: data
+      var proc = module.exports.linxProc.childProcess
+
+      proc.stdout.on('data', function (data) {
+        console.log('[spawn] stdout: ', data.toString())
+        res.status(200).json({
+          status: 'compiled and deployed',
+          port: module.exports.port
         })
       })
 
-      res.status(200).json({
-        status: 'compiled and deployed',
-        port: module.exports.port
+      module.exports.linxProc.then(function (result) {
+        console.log('[spawn] stdout: ', result.stdout.toString())
+        res.status(200).json({
+          status: 'compiled and deployed',
+          port: module.exports.port
+        })
       })
+        .catch(function (err) {
+          console.error('[spawn] stderr: ', err.stderr)
+          res.status(500).json({
+            status: 'compile error',
+            message: err.stderr
+          })
+        })
+      // module.exports.linxProc = execFile('linx', [`--config=tmp/${username}_config`, `tmp/${username}_source.links`], (error, stdout, stderr) => {
+      //   if (error) {
+      //     console.log(error)
+      //   }
+      //   if (stderr) {
+      //     console.log(stderr)
+      //     res.status(500).json({
+      //       status: 'compile error',
+      //       message: stderr
+      //     })
+      //   }
+      //   if (stdout) {
+      //     res.status(200).json({
+      //       status: 'compiled and deployed',
+      //       port: module.exports.port
+      //     })
+      //   }
+      // })
+      // module.exports.linxProc = spawn('linx', [`--config=tmp/${username}_config`, `tmp/${username}_source.links`])
+
+      // if (module.exports.linxProc.stderr.length > 0) {
+      //   const compileError = module.exports.linxProc.stderr.toString()
+      //   console.log(compileError)
+      //   res.status(500).json({
+      //     status: 'compile error',
+      //     message: compileError
+      //   })
+      // } else {
+      //   res.status(200).json({
+      //     status: 'compiled and deployed',
+      //     port: module.exports.port
+      //   })
+      // }
+      // module.exports.linxProc.stderr.('data', (data) => {
+      //   console.log(data.toString())
+      //   res.status(500).json({
+      //     status: 'compile error',
+      //     message: data.toString()
+      //   })
+      // })
+
+      // if (!module.exports.linxProc.killed) {
+      // }
     })
     .catch(error => {
       console.log(error)
