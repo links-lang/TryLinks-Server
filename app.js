cb631098a2220883f0a7202e9ff382154bfd644d
NickWu <wujunao19950715@gmail.com>
2018-03-05 13:50:20 +0000

    Added CORS for personal domain, and updated NPM.

--- app.js
+++ app.js
@@ -29,24 +29,21 @@ app.use(session({
   cookie: {maxAge: 1000 * 60 * 60 * 24}
 }))
 
-// Add headers
 app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://devpractical.com:5000')
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080')
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true)
-
-  // Pass to next layer of middleware
-  next()
+  var allowedOrigins = [
+    'http://localhost:8080',
+    'http://devpractical.com:5000',
+    'http://www.devpractical.com:5000'
+  ]
+  var origin = req.headers.origin
+  if (allowedOrigins.indexOf(origin) > -1) {
+    res.setHeader('Access-Control-Allow-Origin', origin)
+  }
+  // res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
+  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  res.header('Access-Control-Allow-Credentials', true)
+  return next()
 })
 
 app.get('/readme', function (req, res) {
